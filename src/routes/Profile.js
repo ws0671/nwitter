1ded62495ca9b079802ac728e7776c5ea73975c7
ws0671 <ws0671@naver.com>
2023-06-02 16:01:32 +0900

    2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
-
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = async () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return <button onClick={onLogOutClick}>Log out</button>;
+};
 export default Profile;
